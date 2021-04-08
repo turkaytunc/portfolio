2b756f0425e875445c1dc456043a893b80d7c4d1
Türkay Tunç <turkaytunc00@gmail.com>
2021-04-08 14:26:39 +0300

    Add: basic routing

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import { Router } from 'react-router-dom';
+import { createBrowserHistory } from 'history';
 import App from './App';
 
 describe('<App />', () => {
   it('should find hello text', async () => {
-    render(<App />);
+    const history = createBrowserHistory();
+    history.push('/portfolio/');
+    render(
+      <Router history={history}>
+        <App />
+      </Router>
+    );
     const linkElement = await screen.findByText(/hello/i);
     expect(linkElement).toBeInTheDocument();
   });
--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,22 @@
 import React from 'react';
 import './App.scss';
 
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Home from './components/home/Home';
+
 function App() {
-  return <div className="App">hello</div>;
+  return (
+    <BrowserRouter basename="/portfolio">
+      hello
+      <nav>navigation</nav>
+      <main>
+        <Switch>
+          <Route exact path="/" component={Home} />
+        </Switch>
+      </main>
+      <footer>footer</footer>
+    </BrowserRouter>
+  );
 }
 
 export default App;
