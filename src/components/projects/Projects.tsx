8cc0242539c58865493d3be1bab8220736637c64
Türkay Tunç <turkaytunc00@gmail.com>
2021-04-10 22:01:28 +0300

    update: desktop side menu link colors

--- src/components/projects/Projects.tsx
+++ src/components/projects/Projects.tsx
@@ -2,7 +2,14 @@ import React from 'react';
 import './projects.scss';
 
 const Projects = () => {
-  return <div>Projects</div>;
+  return (
+    <section className="projects-container">
+      Projects
+      <section className="projects-section"> area 1</section>
+      <section className="projects-section"> area 2</section>
+      <section className="projects-section"> area 3</section>
+    </section>
+  );
 };
 
 export default Projects;
