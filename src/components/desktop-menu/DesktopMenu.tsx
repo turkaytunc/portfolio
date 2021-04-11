0710340fdfb50afc3e74ef685dc476f1a3757951
Türkay Tunç <turkaytunc00@gmail.com>
2021-04-11 16:42:53 +0300

    add: desktop menu hover animations

--- src/components/desktop-menu/DesktopMenu.tsx
+++ src/components/desktop-menu/DesktopMenu.tsx
@@ -5,7 +5,10 @@ import Navbar from '../navbar/Navbar';
 const DesktopMenu = () => {
   return (
     <aside className="desktop-menu-container">
-      <section className="desktop-menu-photo">Photo - Name</section>
+      <section className="desktop-menu-photo">
+        <p className="desktop-menu-name">Türkay Tunç</p>
+        <p className="desktop-menu-title">Fullstack Web Developer</p>
+      </section>
       <section>
         <Navbar />
       </section>
