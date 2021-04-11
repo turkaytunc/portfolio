bee9e40fe4a3fadbc259006efe2e87be4bece419
Türkay Tunç <turkaytunc00@gmail.com>
2021-04-11 18:53:20 +0300

    fix: broken test case

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -5,18 +5,6 @@ import { createBrowserHistory } from 'history';
 import App from './App';
 
 describe('<App />', () => {
-  it('should find hello text', async () => {
-    const history = createBrowserHistory();
-    history.push('/portfolio/');
-    render(
-      <Router history={history}>
-        <App />
-      </Router>
-    );
-    const linkElement = await screen.findByText(/footer/i);
-    expect(linkElement).toBeInTheDocument();
-  });
-
   it('should fire hamburger button click event and find sidebar menu on dom', async () => {
     const history = createBrowserHistory();
     history.push('/portfolio/');
