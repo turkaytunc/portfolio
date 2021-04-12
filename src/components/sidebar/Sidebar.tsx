d4c4c1b4b200fc388eee7ed3b427aac8ea36fb4e
Türkay Tunç <turkaytunc00@gmail.com>
2021-04-12 19:04:40 +0300

    update: sidebar title

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -7,9 +7,9 @@ const Sidebar = ({ showSidebar }: { showSidebar: boolean }) => {
   const currentClass = showSidebar ? 'sidebar-in' : 'sidebar-out';
   return (
     <aside className={`sidebar-container ${currentClass}`}>
-      <section className="mobile-menu-photo flex-col">
-        <p className="mobile-menu-name text-gray-800">Türkay Tunç</p>
-        <p className="mobile-menu-title text-gray-800">Fullstack Web Developer</p>
+      <section className="mobile-menu-photo flex-col w-full align-middle justify-center text-center">
+        <p className="mobile-menu-name text-gray-800 text-3xl font-bold">Türkay Tunç</p>
+        <p className="mobile-menu-title text-gray-800 text-sm mt-2">Fullstack Web Developer</p>
       </section>
       <Navbar />
       <Footer />
