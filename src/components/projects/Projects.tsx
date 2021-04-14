7bcbb8c0de7da02d037aa593514022edc83c44fa
Türkay Tunç <turkaytunc00@gmail.com>
2021-04-14 11:56:44 +0300

    update: projects section desktop layout

--- src/components/projects/Projects.tsx
+++ src/components/projects/Projects.tsx
@@ -5,21 +5,33 @@ import './projects.scss';
 const Projects = () => {
   const arr = [];
 
-  for (let i = 0; i < 10; i += 1) {
-    arr.push(<Project key={Math.random()} content="random content" />);
+  for (let i = 0; i < 14; i += 1) {
+    arr.push(<Project key={Math.random()} content={`random content ${i}`} />);
   }
 
   return (
     <section className="projects-container">
       <div className="project-container">
         {arr.map((item, i) => {
-          if (i % 2 === 0) return item;
+          if (i < arr.length / 4) return item;
           return null;
         })}
       </div>
       <div className="project-container">
         {arr.map((item, i) => {
-          if (i % 2 !== 0) return item;
+          if (i >= arr.length / 4 && i < arr.length / 2) return item;
+          return null;
+        })}
+      </div>
+      <div className="project-container">
+        {arr.map((item, i) => {
+          if (i >= arr.length / 2 && i < arr.length / (4 / 3)) return item;
+          return null;
+        })}
+      </div>
+      <div className="project-container">
+        {arr.map((item, i) => {
+          if (i > arr.length / (4 / 3)) return item;
           return null;
         })}
       </div>
