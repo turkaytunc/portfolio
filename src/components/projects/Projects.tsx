6300f8b27be478a727007d5c2f858ecf0ebcfb9f
Türkay Tunç <turkaytunc00@gmail.com>
2021-04-15 12:27:34 +0300

    refactor: projects compoennt, now project data comes from json file instead

--- src/components/projects/Projects.tsx
+++ src/components/projects/Projects.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import projectsData from 'src/utils/projectsData.json';
 import Project from './project/Project';
 import './projects.scss';
 
@@ -6,97 +7,15 @@ const Projects = () => {
   return (
     <div className="projects-container">
       <div className="projects-row">
-        <Project
-          title="Pelp"
-          imageSource="pelp.png"
-          techs={['Docker', 'TravisCI', 'Reactjs', 'Jest', 'PostgreSql', 'Typescript', 'Heroku', 'Nodejs']}
-          content="this is a content"
-          githubLink="https://github.com/turkaytunc/Racon"
-          liveProjectLink="https://turkaytunc.itch.io/racon"
-        />
-        <Project
-          title="E-Secim"
-          imageSource="e-secim.png"
-          techs={['TravisCI', 'Reactjs', 'Jest', 'Typescript', 'Sass']}
-          content="this is a content"
-          githubLink="https://github.com/turkaytunc/Racon"
-          liveProjectLink="https://turkaytunc.itch.io/racon"
-        />
-        <Project
-          title="Mein Shop"
-          imageSource="mein-shop.png"
-          techs={['Reactjs', 'Jest', 'Javascript', 'Redux']}
-          content="this is a content"
-          githubLink="https://github.com/turkaytunc/Racon"
-          liveProjectLink="https://turkaytunc.itch.io/racon"
-        />
-        <Project
-          title="Redux-like"
-          imageSource="redux-like.png"
-          techs={['Reactjs', 'Typescript']}
-          content="this is a content"
-          githubLink="https://github.com/turkaytunc/Racon"
-          liveProjectLink="https://turkaytunc.itch.io/racon"
-        />
-        <Project
-          title="Wapp"
-          imageSource="weather-app.png"
-          techs={['Redux', 'Reactjs', 'Javascriptscript']}
-          content="this is a content"
-          githubLink="https://github.com/turkaytunc/Racon"
-          liveProjectLink="https://turkaytunc.itch.io/racon"
-        />
-        {/* fix above  links */}
-        <Project
-          title="Markdown Previewer"
-          imageSource="md-prev.png"
-          techs={['Sass', 'Regex', 'Reactjs', 'TravisCI', 'Jest']}
-          content="this is a content"
-          githubLink="https://github.com/turkaytunc/md-prev"
-          liveProjectLink="https://turkaytunc.github.io/md-prev/"
-        />
-        <Project
-          title="Study Timer"
-          imageSource="study-timer.png"
-          techs={['Sass', 'Jest', 'Reactjs', 'TravisCI', 'Codecov']}
-          content="this is a content"
-          githubLink="https://github.com/turkaytunc/study-timer"
-          liveProjectLink="https://turkaytunc.github.io/study-timer/"
-        />
-
-        <Project
-          title="Random Quote Machine"
-          imageSource="random-quote-machine.png"
-          techs={['Sass', 'Jest', 'Reactjs', 'TravisCI', 'Coveralls']}
-          content="this is a content"
-          githubLink="https://github.com/turkaytunc/random-quote-machine"
-          liveProjectLink="https://turkaytunc.github.io/random-quote-machine/"
-        />
-
-        <Project
-          title="Restful API"
-          imageSource="rest.jpg"
-          techs={['MongoDB', 'Mongoose', 'Nodejs']}
-          content="this is a content"
-          githubLink="https://github.com/turkaytunc/rest-api-practice"
-        />
-
-        <Project
-          title="First Person Character Controller"
-          imageSource="fps-controller.png"
-          techs={['C#', 'Unity']}
-          content="this is a content"
-          githubLink="https://github.com/turkaytunc/3D-character-controller"
-        />
-
-        <Project
-          title="Space Shooter"
-          imageSource="space-shooter.png"
-          techs={['GIMP', 'Aseprite', 'Unity', 'C#', 'GLSL']}
-          content="this is a content"
-          githubLink="https://github.com/turkaytunc/Racon"
-          liveProjectLink="https://turkaytunc.itch.io/racon"
-        />
+        {projectsData.map((item) => (
+          <Project
+            title={item.title}
+            imageSource={item.imageSource}
+            techs={item.techs}
+            githubLink={item.githubLink}
+            liveProjectLink={item.liveProjectLink}
+          />
+        ))}
       </div>
     </div>
   );
--- src/components/projects/project/Project.tsx
+++ src/components/projects/project/Project.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import './project.scss';
 
 type ProjectProp = {
-  content: string;
   imageSource: string;
   title: string;
   techs: string[];
@@ -11,16 +10,23 @@ type ProjectProp = {
   zIndex?: number;
 };
 
-const Project = ({ zIndex = 0, content, imageSource, githubLink, techs, title, liveProjectLink }: ProjectProp) => {
+const Project = ({ zIndex = 0, imageSource, githubLink, techs, title, liveProjectLink }: ProjectProp) => {
   return (
     <div className="project-card" style={{ zIndex: zIndex! }}>
       <header>Project Name</header>
-      {content}
+
       <div className="project-image-container max-h-36 max-w-36 rounded-md lg:max-h-60 lg:max-w-60 overflow-hidden">
         <img src={`${process.env.PUBLIC_URL}/images/${imageSource}`} alt="project" />
       </div>
-      <div>Tech Used</div>
-      <div>github and live project</div>
+      <div>
+        {techs.map((item) => (
+          <div key={Math.random()}>{item}</div>
+        ))}
+      </div>
+      <div>
+        <div>{githubLink}</div>
+        <div>{}</div>
+      </div>
     </div>
   );
 };
